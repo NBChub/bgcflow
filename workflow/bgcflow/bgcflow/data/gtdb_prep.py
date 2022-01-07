a0f0d99bd7ed74c439b4c1450b640e2ed9120b66
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2022-01-07 12:36:06 +0100

    fix: enable missing reference placement

--- workflow/bgcflow/bgcflow/data/gtdb_prep.py
+++ workflow/bgcflow/bgcflow/data/gtdb_prep.py
@@ -6,17 +6,19 @@ def gtdb_prep(samples_path, outfile):
     # wrap single or multiple inputs & generate dataframe
     shell_input = samples_path.replace("[","").replace("]","")
     dfList = [pd.read_csv(s).set_index('genome_id', drop=False) for s in shell_input.split()]
-    df_samples = pd.concat(dfList, axis=0)
+    df_samples = pd.concat(dfList, axis=0).fillna("")
 
     # NCBI accession or closest NCBI
     custom_placement = df_samples[df_samples.source.eq("custom")].closest_placement_reference.tolist()
-    ncbi_placement = df_samples[df_samples.source.eq("ncbi")].genome_id.tolist()
+    ncbi_placement = df_samples[df_samples.source.eq("ncbi")].genome_id.tolist() # only works for GCF
     patric_placement = df_samples[df_samples.source.eq("patric")].closest_placement_reference.tolist()
     placement_tax = custom_placement + ncbi_placement + patric_placement
-
     with open(outfile, "w") as out:
         for acc in placement_tax:
-            out.write(acc + "\n")
+            if acc == "":
+                pass
+            else:
+                out.write(acc + "\n")
         out.close()    
     return None
 
