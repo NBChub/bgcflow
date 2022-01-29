a42fcb5c8d0d8cc5fe2903ac2faecaabaa4fd6bb
OmkarSaMo <omkmoh.biosustain.dtu.dk>
2022-01-29 18:10:19 +0100

    feat: new checkm rule for genome QA

---
+++ workflow/bgcflow/bgcflow/data/get_checkm_data.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pandas as pd
+import json
+
+def get_checkm_data(checkm_input_stats, checkm_json_folder, checkm_output_stats):
+    '''
+    Read checkm output stats.tsv file, return json output for individual genomes and output stats table
+
+    Parameters
+    ---------- 
+    1. checkm_input_stats : str / path
+        Location of the output from checkm software 'storage/bin_stats_ext.tsv'
+    2. checkm_json_folder : str / path
+        Location of the json folder to store output for each genome
+    3. checkm_output_stats : str / path
+        Location of the output csv file in the processed/tables directory
+
+    Returns
+    -------
+    1. json : .json file
+        A json file for each genome summarizing the checkm output 
+    2. checkm_output_stats : str / path
+        Output csv file in the processed/tables directory with checkm information for all genomes
+    '''
+
+    # List of columns in df_checkm_stats.csv
+    checkm_columns = ['Completeness', 'Contamination', 'GC', 'GC std', 'Genome size', '# ambiguous bases', '# scaffolds', 
+                '# contigs', 'Longest scaffold', 'Longest contig', 'N50 (scaffolds)', 'N50 (contigs)', 'Mean scaffold length', 
+                'Mean contig length', 'Coding density', 'Translation table', '# predicted genes']
+    
+    df_checkm_input = pd.read_csv(checkm_input_stats, sep='\t',header=None, index_col=0)
+
+    df_checkm_out = pd.DataFrame(index=df_checkm_input.index, columns=checkm_columns)
+    df_checkm_out.index.name = 'genome_id'
+    
+    for genome_id in df_checkm_out.index:
+        json_genome_id = json.loads(df_checkm_input.loc[genome_id,1].replace("'",'"'))
+        json_path = os.path.join(checkm_json_folder, genome_id + '.json')
+
+        with open(json_path, "w") as outfile:
+            json.dump(json_genome_id, outfile)
+        for col in df_checkm_out.columns:
+            df_checkm_out.loc[genome_id, col] = json_genome_id[col]
+        
+    df_checkm_out.to_csv(checkm_output_stats)
+    
+    return df_checkm_out
+
+if __name__ == "__main__":
+    get_checkm_data(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ workflow/rules/checkm.smk
@@ -0,0 +1,49 @@
+rule install_checkm:
+    output:
+        checkm_db = directory("resources/checkm/")
+    conda:
+        "../envs/checkm.yaml"
+    log: "workflow/report/logs/checkm/checkm-install_checkm.log"
+    shell:
+        """
+        (cd resources && wget https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.tar.gz) > {log}
+        (cd resources && mkdir -p checkm && tar -xvf checkm_data_2015_01_16.tar.gz -C checkm && rm checkm_data_2015_01_16.tar.gz)
+        checkm data setRoot resources/checkm > {log}
+        """
+
+rule checkm:
+    input:
+        _all_ = expand("data/interim/fasta/{strains}.fna", strains=STRAINS),
+        fna = "data/interim/fasta",
+        checkm_db = "resources/checkm/",
+    output:
+        stat = "data/interim/checkm/storage/bin_stats_ext.tsv",
+        checkm_dir = directory("data/interim/checkm/"),
+        bins = expand("data/interim/checkm/bins/{strains}/genes.faa", strains=STRAINS),
+    conda:
+        "../envs/checkm.yaml"
+    log: "workflow/report/logs/checkm/checkm.log"
+    params: 
+        checkm_log = "data/interim/checkm/checkm.log",
+    threads: 64
+    shell:
+        """
+        checkm lineage_wf -t {threads} --reduced_tree -x fna {input.fna} {output.checkm_dir}
+        cat {params.checkm_log} >> {log}
+        """
+
+rule checkm_out:
+    input:
+        stat = "data/interim/checkm/storage/bin_stats_ext.tsv",
+        _all_ = expand("data/interim/checkm/bins/{strains}/genes.faa", strains=STRAINS),
+    output:
+        stat_processed = "data/proceesed/tables/checkm_stats.tsv",
+        checkm_json = directory("data/interim/checkm/json/"),
+        json_all = expand("data/interim/checkm/json/{strains}.json", strains=STRAINS),
+    log: "workflow/report/logs/checkm/checkm_out.log"
+    conda:
+        "../envs/bgc_analytics.yaml"
+    shell:
+        """
+        python workflow/bgcflow/bgcflow/data/get_checkm_data.py {input.stat} {output.checkm_json} {output.stat_processed} 2>> {log}
+        """
