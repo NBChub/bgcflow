5bea524c5bb8779c498b0819b214582ce2d38bf4
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2022-04-05 06:39:53 +0000

    fix: deal with unassigned taxonomy in GTDB - GCA_003223065.1

--- workflow/bgcflow/bgcflow/data/fix_gtdb_taxonomy.py
+++ workflow/bgcflow/bgcflow/data/fix_gtdb_taxonomy.py
@@ -35,20 +35,26 @@ def summarize_gtdb_json(accession_list, df_gtdb_output):
     df_taxonomy.columns = [c.title() for c in df_taxonomy.columns]
 
     # Getting other metadata
-    logging.info("Getting metadata into table...")
-    metadata = pd.DataFrame.from_dict({i : df.loc[i, 'metadata'] for i in df.index}).T
+    try:
+        logging.info("Getting metadata into table...")
+        metadata = pd.DataFrame.from_dict({i : df.loc[i, 'metadata'] for i in df.index}).T
 
-    metadata_container = []
-    for c in metadata.columns:
-        value = {i : metadata.loc[i, c] for i in metadata.index}
-        try:
-            metadata_container.append(pd.DataFrame.from_dict(value).T)
-        except ValueError:
-            metadata_container.append(pd.DataFrame([value]).T.rename(columns = {0 : c}))
-    df_metadata = pd.concat(metadata_container, axis=1)
+        metadata_container = []
+        for c in metadata.columns:
+            value = {i : metadata.loc[i, c] for i in metadata.index}
+            try:
+                metadata_container.append(pd.DataFrame.from_dict(value).T)
+            except ValueError:
+                metadata_container.append(pd.DataFrame([value]).T.rename(columns = {0 : c}))
+        df_metadata = pd.concat(metadata_container, axis=1)
 
-    logging.info("Finalizing table...")
-    df_final = pd.concat([df.loc[:, ["genome_id", "gtdb_release"]], df_taxonomy, df_metadata], axis=1)
+        logging.info("Finalizing table...")
+        df_final = pd.concat([df.loc[:, ["genome_id", "gtdb_release"]], df_taxonomy, df_metadata], axis=1)
+
+    except KeyError:
+        logging.info("No additional metadata found.")
+        logging.info("Finalizing table...")
+        df_final = pd.concat([df.loc[:, ["genome_id", "gtdb_release"]], df_taxonomy], axis=1)
 
     # save to file
     logging.info(f"Writing to file: {df_gtdb_output}")
