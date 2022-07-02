514599fe71c2bf115fe372c7cb4d9f9a6328a2b4
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2022-07-02 14:55:33 +0200

    fix: correct date selection of project query runs

--- workflow/bgcflow/bgcflow/data/get_bigslice_query_result.py
+++ workflow/bgcflow/bgcflow/data/get_bigslice_query_result.py
@@ -3,27 +3,39 @@ import pandas as pd
 from pathlib import Path
 import shutil
 import sys
+import logging
+
+log_format = '%(levelname)-8s %(asctime)s   %(message)s'
+date_format = "%d/%m %H:%M:%S"
+logging.basicConfig(format=log_format, datefmt=date_format, level=logging.DEBUG)
 
 def get_bigslice_query(project_name, output_folder, result_folder="resources/bigslice/full_run_result/"):
     bigslice_full_run = Path(result_folder)
     query_id = find_query_result(project_name, bigslice_full_run)
     fetch_query_tables(query_id, output_folder, bigslice_full_run)
-    print("copying sql database...")
+    logging.info("Copying SQL database of the run...")
     shutil.copy(bigslice_full_run / f"reports/{query_id}/data.db", Path(output_folder) / f"{query_id}.db")
+    logging.info("Job done!")
     return
 
 def find_query_result(project_name, bigslice_full_run):
+    logging.info("Connecting to BiG-SLICE SQL Reports...")
     conn = sqlite3.connect(bigslice_full_run / "reports/reports.db")
     df = pd.read_sql_query(f"select * from reports;", conn)
 
     # select only run result of the project
-    filter_name = df.name == project_name
-    df = df.where(filter_name).dropna()
+    match = []
+    for i in df.index:
+        if project_name in df.loc[i, "name"]:
+            match.append(i)
+    df = df.loc[match, :]
+    logging.debug(f"Found resulting match to project {project_name}:\n{df}")
     
     # select the latest run for the project
     df['creation_date'] = pd.to_datetime(df['creation_date'])
     filter_time = df.creation_date == df.creation_date.max()
     df = df.where(filter_time).dropna()
+    logging.debug(f"Extracting information from the latest run:{df.name.values}")
     return int(df.id.values)
 
 def fetch_query_tables(query_id, output_folder, bigslice_full_run):
@@ -40,11 +52,11 @@ def fetch_query_tables(query_id, output_folder, bigslice_full_run):
     try:
         output_folder.mkdir(parents=True, exist_ok=False)
     except FileExistsError:
-        print(f"Directory {output_folder} already exist")
+        logging.debug(f"Directory {output_folder} already exist")
     else:
-        print(f"Creating directory: {output_folder}...")
+        logging.debug(f"Creating directory: {output_folder}...")
     
-    print(f"Writing results to {output_folder}...")
+    logging.debug(f"Extracting tables to {output_folder}...")
     
     # convert table to pandas df
     df_tables = {}
@@ -54,7 +66,6 @@ def fetch_query_tables(query_id, output_folder, bigslice_full_run):
         df.to_csv(outfile, index=False)
         df_tables.update({t : df})
     return
-
     
 if __name__ == "__main__":
     get_bigslice_query(sys.argv[1], sys.argv[2], sys.argv[3])
