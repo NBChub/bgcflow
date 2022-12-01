ecaef7fe7fa707b3fe2c4c0cd773499ccb9165ee
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2022-12-01 11:44:54 +0000

    feat: update ARTS2 for python3

--- workflow/envs/arts.post-deploy.sh
+++ workflow/envs/arts.post-deploy.sh
@@ -7,10 +7,19 @@ set -e
 mkdir -p workflow/report/logs/arts
 
 # get the latest arts release
-wget -P resources/ https://bitbucket.org/ziemertlab/arts/get/master.tar.gz 2>> workflow/report/logs/arts/arts_setup.log
-(cd resources/ && tar -xvzf master.tar.gz) 2>> workflow/report/logs/arts/arts_setup.log
-mv resources/ziemertlab-arts-* resources/arts 2>> workflow/report/logs/arts/arts_setup.log
+rm -rf resources/arts 2> workflow/report/logs/arts/arts_setup.log
+git clone git@github.com:NBChub/arts_v3.git resources/arts &> workflow/report/logs/arts/arts_setup.log
 mkdir -p data/interim/arts/tmp/ 2>> workflow/report/logs/arts/arts_setup.log
+
+# set up reference database
+wget -P resources/ https://bitbucket.org/ziemertlab/arts/get/master.tar.gz -nc 2>> workflow/report/logs/arts/arts_setup.log
+(cd resources/ && tar -xvzf master.tar.gz ziemertlab-arts-b4789c6b3a88/reference) 2>> workflow/report/logs/arts/arts_setup.log
+mv resources/ziemertlab-arts-*/reference resources/arts/ 2>> workflow/report/logs/arts/arts_setup.log
+(cd resources/ && tar -xvzf master.tar.gz ziemertlab-arts-b4789c6b3a88/astral) 2>> workflow/report/logs/arts/arts_setup.log
+mv resources/ziemertlab-arts-*/astral resources/arts/ 2>> workflow/report/logs/arts/arts_setup.log
+rm resources/master.tar.gz
+
+# set up environment variable
 echo "ARTS_RESULTS=data/interim/arts/tmp/" > resources/arts/.env
 echo "ARTS_UPLOAD=data/interim/arts/tmp/" >> resources/arts/.env
 echo "ARTS_RUN=data/interim/arts/tmp/" >> resources/arts/.env
