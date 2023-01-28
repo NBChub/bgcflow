7cae028e0ac3e8c40364d238edc6a9e16c0974e4
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-01-28 11:25:48 +0000

    test: add antismash_overview_gather test

---
+++ .tests/unit/test_antismash_overview_gather.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_antismash_overview_gather():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/antismash_overview_gather/data")
+        expected_path = PurePosixPath(".tests/unit/antismash_overview_gather/expected")
+
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("data/processed/Lactobacillus_delbrueckii/tables/df_antismash_6.1.1_bgc.csv", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "data/processed/Lactobacillus_delbrueckii/tables/df_antismash_6.1.1_bgc.csv",
+            "-f", 
+            "-j1",
+            "--keep-target-files",
+
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
