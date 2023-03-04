ad4692099ce7c7dec473f4fa33327fc6bb3a95ea
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-03-04 19:41:38 +0000

    test: fix test change and visibility

--- .tests/unit/test_antismash_overview_gather.py
+++ .tests/unit/test_antismash_overview_gather.py
@@ -1,14 +1,13 @@
 import os
-import sys
-
-import subprocess as sp
-from tempfile import TemporaryDirectory
 import shutil
+import subprocess as sp
+import sys
 from pathlib import Path, PurePosixPath
-
-sys.path.insert(0, os.path.dirname(__file__))
+from tempfile import TemporaryDirectory
 
 import common
+
+sys.path.insert(0, os.path.dirname(__file__))
 
 
 def test_antismash_overview_gather():
@@ -21,24 +20,28 @@ def test_antismash_overview_gather():
         shutil.copytree(data_path, workdir)
 
         # dbg
-        print("data/processed/Lactobacillus_delbrueckii/tables/df_antismash_6.1.1_bgc.csv", file=sys.stderr)
+        print(
+            "data/processed/Lactobacillus_delbrueckii/tables/df_antismash_6.1.1_bgc.csv",
+            file=sys.stderr,
+        )
 
         # Run the test job.
-        sp.check_output([
-            "python",
-            "-m",
-            "snakemake", 
-            "data/processed/Lactobacillus_delbrueckii/tables/df_antismash_6.1.1_bgc.csv",
-            "-f", 
-            "-j1",
-            "--keep-target-files",
-
-            "--directory",
-            workdir,
-        ])
+        sp.check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "data/processed/Lactobacillus_delbrueckii/tables/df_regions_antismash_6.1.1.csv",
+                "-f",
+                "-j1",
+                "--keep-target-files",
+                "--directory",
+                workdir,
+            ]
+        )
 
         # Check the output byte by byte using cmp.
         # To modify this behavior, you can inherit from common.OutputChecker in here
-        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # and overwrite the method `compare_files(generated_file, expected_file),
         # also see common.py.
         common.OutputChecker(data_path, expected_path, workdir).check()
