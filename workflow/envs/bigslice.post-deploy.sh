ecb0716321cc6d5939b0aadead9b4252f6a71bbd
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-07-06 08:13:51 +0200

    feat: upgrade bigslice for compatibility with antiSMASH7

--- workflow/envs/bigslice.post-deploy.sh
+++ workflow/envs/bigslice.post-deploy.sh
@@ -4,9 +4,13 @@
 set -e
 
 # create log folder
-mkdir -p workflow/report/logs/bigslice
-LOG='workflow/report/logs/bigslice/install_bigslice.log'
+LOG_DIR="logs/bigslice"
+mkdir -p logs/bigslice
+LOG="$LOG_DIR/install_bigslice.log"
 
 # get the bigslice models
-(cd resources && download_bigslice_hmmdb && rm bigslice_models.tar.gz) 2>> $LOG
+wget -O resources/bigslice_models.tar.gz https://github.com/medema-group/bigslice/releases/download/v1.0.0/bigslice-models.2020-04-27.tar.gz 2> $LOG
+mkdir -p $CONDA_PREFIX/bin/bigslice-models 2>> $LOG
+tar -xvf resources/bigslice_models.tar.gz -C $CONDA_PREFIX/bin/bigslice-models 2>> $LOG
+rm resources/bigslice_models.tar.gz 2>> $LOG
 bigslice --version &>> $LOG
