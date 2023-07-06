30b86bbdd003ecbf8359221422cf7b51c32dd166
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-07-06 09:30:18 +0200

    test: update test for 0.7.0

---
+++ .tests/unit/test_antismash_overview.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_antismash_overview():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/antismash_overview/data")
+        expected_path = PurePosixPath(".tests/unit/antismash_overview/expected")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("data/interim/antismash/7.0.0/GCA_000056065.1_bgc_overview.json", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "data/interim/antismash/7.0.0/GCA_000056065.1_bgc_overview.json",
+            "-f", 
+            "-j1",
+            "--keep-target-files",
+    
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
