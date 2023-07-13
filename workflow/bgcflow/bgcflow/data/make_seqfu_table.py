6234db3868fdd93c9ad1c16097ddc8d2f9f3cece
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-07-13 21:27:12 +0000

    feat: pass input to text to handle long input for seqfu

--- workflow/bgcflow/bgcflow/data/make_seqfu_table.py
+++ workflow/bgcflow/bgcflow/data/make_seqfu_table.py
@@ -16,11 +16,39 @@ def gather_seqfu_json(seqfu_input, output_file):
     Merge seqfu json input into one csv table
     """
     # Accomodate multiple inputs to generate dataframe
-    shell_input = seqfu_input.split()
-    logging.info("Merging seqfu results...")
+    shell_input = Path(seqfu_input)
+    logging.info(shell_input)
+    if shell_input.is_file() and shell_input.suffix == ".json":
+        logging.info(f"Getting SeqFu overview from a single file: {shell_input}")
+        shell_input_files = shell_input
+
+    elif shell_input.is_file() and shell_input.suffix == ".txt":
+        logging.info(f"Getting SeqFu overview  from a text file: {shell_input}")
+        with open(shell_input, "r") as file:
+            file_content = [i.strip("\n") for i in file.readlines()]
+            if len(file_content) == 1:
+                # Paths space-separated on a single line
+                paths = file_content[0].split()
+            else:
+                # Paths written on separate lines
+                paths = file_content
+            shell_input_files = [
+                Path(path) for path in paths if Path(path).suffix == ".json"
+            ]
+    else:
+        shell_input_files = [
+            Path(file)
+            for file in str(shell_input).split()
+            if Path(file).suffix == ".json"
+        ]
+        logging.info(
+            f"Getting SeqFu overview from the given list of {len(shell_input_files)} files..."
+        )
+
+    logging.info("Merging SeqFu results...")
     container = {}
-    with alive_bar(len(shell_input), title="Updating BGC information:") as bar:
-        for d in shell_input:
+    with alive_bar(len(shell_input_files), title="Updating SeqFu information:") as bar:
+        for d in shell_input_files:
             logging.debug(f"Reading input: {Path(d).name}")
             with open(d, "r") as f:
                 read = json.load(f)
