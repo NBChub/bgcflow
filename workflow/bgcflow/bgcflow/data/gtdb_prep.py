c45b047c3788c0f8fdc499470ea001904a4a6d00
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2023-10-16 11:54:36 +0200

    fix: set input value as bool for offline mode

--- workflow/bgcflow/bgcflow/data/gtdb_prep.py
+++ workflow/bgcflow/bgcflow/data/gtdb_prep.py
@@ -52,8 +52,8 @@ def gtdb_prep(
         """
         Helper script to decide taxonomic placement for a given query
         """
-        logging.info(f"Offline mode: {offline_mode}")
-        if offline_mode is True:
+        logging.info(f"Offline mode: {bool(offline_mode)}")
+        if bool(offline_mode) is True:
             logging.info(
                 "WARNING: Running in offline mode. Returning empty dataframe..."
             )
