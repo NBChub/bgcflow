096a9254b1a4147e9ec024c9227a4715908017b8
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-04-03 13:45:26 +0000

    fix: improve symlink generation for downsteram BGC prep and sanitize illegal character for BiG-SCAPE

--- workflow/bgcflow/bgcflow/data/bgc_downstream_prep_selection.py
+++ workflow/bgcflow/bgcflow/data/bgc_downstream_prep_selection.py
@@ -10,34 +10,26 @@ date_format = "%d/%m %H:%M:%S"
 logging.basicConfig(format=log_format, datefmt=date_format, level=logging.DEBUG)
 
 
-def bgc_downstream_prep(input_dir, output_dir, selected_bgcs=False):
+def generate_symlink(path, genome_id, output_dir, selected_bgcs=False):
     """
     Given an antiSMASH directory, check for changed name
     """
-    logging.info(f"Reading input directory: {input_dir}")
-    path = Path(input_dir)
-    if not path.is_dir():
-        raise FileNotFoundError(f"No such file or directory: {path}")
-
-    genome_id = path.name
     outpath = Path(output_dir) / genome_id
     outpath.mkdir(parents=True, exist_ok=True)
     logging.debug(f"Deducting genome id as {genome_id}")
-
-    change_log = {genome_id: {}}
     ctr = 0
-    matches = [Path(i).stem for i in selected_bgcs.split()]
+    matches = selected_bgcs.stem
     for gbk in path.glob("*.gbk"):
         if gbk.stem in matches:
-            logging.debug(f"MATCH: {gbk.stem}")
+            logging.debug(f"Found match: {gbk.stem}")
             filename = gbk.name
             ctr = ctr + 1
             logging.info(f"Parsing file: {gbk.name}")
             region = SeqIO.parse(str(gbk), "genbank")
             for record in region:
-                logging.info(f"{gbk} {record.id}")
+                logging.debug(f"Processing: {gbk.name}: {record.id}")
                 record_log = {}
-                if "comment" in record.annotations:
+                if "structured_comment" in record.annotations:
                     try:
                         original_id = record.annotations["structured_comment"][
                             "antiSMASH-Data"
@@ -47,7 +39,35 @@ def bgc_downstream_prep(input_dir, output_dir, selected_bgcs=False):
                         logging.warning(
                             f"Found shortened record.id: {record.id} <- {original_id}."
                         )
+                else:
+                    raise ValueError(f"No Structured Comments in record: {gbk.name}")
 
+                if (":" in str(record.description)) or (":" in original_id):
+                    logging.warning(
+                        f"Illegal character ':' found in genbank description, removing: {record.description}"
+                    )
+                    # Remove colon from description
+                    record.description = record.description.replace(":", "")
+                    original_id = original_id.replace(":", "")
+
+                    # Rename antiSMASH comment
+                    if "structured_comment" in record.annotations:
+                        if (
+                            "Original ID"
+                            in record.annotations["structured_comment"][
+                                "antiSMASH-Data"
+                            ]
+                        ):
+                            record.annotations["structured_comment"]["antiSMASH-Data"][
+                                "Original ID"
+                            ] = original_id
+
+                    # Write new GenBank file
+                    new_filename = filename.replace(record.id, original_id)
+                    with open(outpath / new_filename, "w") as output_handle:
+                        SeqIO.write(record, output_handle, "genbank")
+                    link = outpath / new_filename
+                else:
                     # generate symlink
                     new_filename = filename.replace(record.id, original_id)
                     target_path = Path.cwd() / gbk  # target for symlink
@@ -64,23 +84,66 @@ def bgc_downstream_prep(input_dir, output_dir, selected_bgcs=False):
                         link.unlink()
                         link.symlink_to(target_path)
 
-                    record_log["record_id"] = record.id
-                    record_log["original_id"] = original_id
-                    record_log["target_path"] = str(gbk)
-                    record_log["symlink_path"] = str(link)
-                else:
-                    logging.warning(f"No Comments in record: {gbk.name}")
+                    # Assert that the symlink was correctly generated
+                    assert link.is_symlink(), f"Failed to create symlink: {link}"
+                    assert (
+                        link.resolve() == target_path
+                    ), f"Symlink {link} does not point to the correct target: {target_path}"
 
-                change_log[genome_id][filename] = record_log
-    # assert 1+1==3
-    with open(
-        outpath / f"{genome_id}-change_log.json", "w", encoding="utf8"
-    ) as json_file:
-        json.dump(change_log, json_file, indent=4)
+                record_log["record_id"] = record.id
+                record_log["original_id"] = original_id
+                record_log["target_path"] = str(gbk)
+                record_log["symlink_path"] = str(link)
 
-    logging.info(f"{genome_id}: Job done!\n")
-    return
+                change_log = {filename: record_log}
+    return change_log
+
+
+def bgc_downstream_prep(input_file, output_dir):
+    logging.info(f"Reading input file: {input_file}")
+    with open(input_file, "r") as file:
+        file_paths = [Path(f) for f in file.read().splitlines()]
+    change_log_containers = {}
+    for num, selected_bgcs in enumerate(file_paths):
+        input_dir = selected_bgcs.parent
+        logging.info(f"Reading input directory: {input_dir}")
+        path = Path(input_dir)
+        if not path.is_dir():
+            raise FileNotFoundError(f"No such file or directory: {path}")
+
+        # check if it has complete antiSMASH results
+        if (path / f"{path.name}.json").is_file():
+            logging.info("Found full antiSMASH record")
+            genome_id = path.name
+        else:
+            logging.warning("No full antiSMASH record found, unknown genome id")
+            genome_id = "unknown_genome_id"
+
+        assert selected_bgcs.exists(), f"File does not exist: {selected_bgcs}"
+        region_change_log = generate_symlink(path, genome_id, output_dir, selected_bgcs)
+        change_log_containers[num] = {
+            "genome_id": genome_id,
+            "value": region_change_log,
+        }
+    change_logs = {}
+    genome_ids = set(v["genome_id"] for v in change_log_containers.values())
+    for genome_id in genome_ids:
+        change_log = {}
+        for v in change_log_containers.values():
+            if v["genome_id"] == genome_id:
+                entry_name = list(v["value"].keys())[0]
+                change_log[entry_name] = v["value"][entry_name]
+        change_logs[genome_id] = change_log
+    logging.debug(change_logs)
+
+    for genome_id in change_logs.keys():
+        outpath = Path(output_dir) / genome_id
+        with open(
+            outpath / f"{genome_id}-change_log.json", "w", encoding="utf8"
+        ) as json_file:
+            json.dump({genome_id: change_logs[genome_id]}, json_file, indent=4)
+        logging.info(f"{genome_id}: Job done!\n")
 
 
 if __name__ == "__main__":
-    bgc_downstream_prep(sys.argv[1], sys.argv[2], sys.argv[3])
+    bgc_downstream_prep(sys.argv[1], sys.argv[2])
