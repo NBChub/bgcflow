0e869820c6d129d12a8c91071b345167bac10558
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-07-05 16:02:40 +0200

    feat: build antismash db before running dbt

--- workflow/envs/antismash_db-duckdb.post-deploy.sh
+++ workflow/envs/antismash_db-duckdb.post-deploy.sh
@@ -4,23 +4,36 @@ set -e
 mkdir -p resources
 rm -rf resources/antismash_db-schema_duckdb
 
+# create logs directory
+mkdir -p logs/antismash_db-duckdb
+LOG="logs/antismash_db-duckdb/antismash_db-duckdb_template.log"
+
 # Clone the antismash_db-schema_duckdb repository into the resources directory
-git clone https://github.com/NBChub/antismash_db-schema_duckdb.git resources/antismash_db-schema_duckdb
+git clone https://github.com/NBChub/antismash_db-schema_duckdb.git resources/antismash_db-schema_duckdb 2>> $LOG
 
 # Clone the db-schema repository into the antismash_db-schema_duckdb directory
-git clone https://github.com/antismash/db-schema.git resources/antismash_db-schema_duckdb/db-schema
+git clone https://github.com/antismash/db-schema.git resources/antismash_db-schema_duckdb/db-schema 2>> $LOG
 
 # Download the antiSMASH databases using a custom script or command
-download-antismash-databases
+download-antismash-databases 2>> $LOG
+
+# Initialize the DuckDB database with the schema from the db-schema directory
+(cd resources/antismash_db-schema_duckdb && python init_duckdb.py db-schema duckdb-schema) 2>> $LOG
 
 # Download the NCBI taxonomy dump files into the ncbi-taxdump directory
-(cd resources/antismash_db-schema_duckdb && wget -P ncbi-taxdump https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz -nc)
+(cd resources/antismash_db-schema_duckdb && wget -P ncbi-taxdump https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz -nc) 2>> $LOG
 
 # Extract the downloaded NCBI taxonomy dump files
-(cd resources/antismash_db-schema_duckdb/ncbi-taxdump && tar -xvf new_taxdump.tar.gz)
+(cd resources/antismash_db-schema_duckdb/ncbi-taxdump && tar -xvf new_taxdump.tar.gz) 2>> $LOG
+
+# Install the asdb-taxa tool using cargo (Rust's package manager)
+cargo install asdb-taxa 2>> $LOG
+
+# Add cargo's bin directory to the PATH to ensure asdb-taxa can be executed
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # Clone the db-import repository into the antismash_db-schema_duckdb directory
-git clone git@github.com:matinnuhamunada/db-import.git resources/antismash_db-schema_duckdb/db-import
+git clone git@github.com:matinnuhamunada/db-import.git resources/antismash_db-schema_duckdb/db-import 2>> $LOG
 
 # Checkout a specific branch of the db-import repository
-(cd resources/antismash_db-schema_duckdb/db-import && git checkout -b v4.0.0-duckdb v4.0.0-duckdb)
+(cd resources/antismash_db-schema_duckdb/db-import && git checkout -b v4.0.0-duckdb v4.0.0-duckdb) 2>> $LOG
--- workflow/rules/antismash-db-duckdb.smk
+++ workflow/rules/antismash-db-duckdb.smk
@@ -1,24 +1,5 @@
-rule antismash_db_duckdb_schema:
-    input:
-        duckdb = "data/processed/{name}/dbt/antiSMASH_{version}/dbt_bgcflow.duckdb"
-    output:
-        duckdb = temp(directory("data/processed/{name}/antismash_database/antiSMASH_{version}_DB_SCHEMA"))
-    conda:
-        "../envs/antismash_db-duckdb.yaml"
-    log:
-        "logs/antismash_db-duckdb_schema/{name}_{version}.log"
-    shell:
-        """
-        duckdb_dir=$PWD/{output.duckdb}
-        mkdir -p $duckdb_dir
-        duckdb=$PWD/{output.duckdb}/antismash_db.duckdb
-        cp {input.duckdb} $duckdb 2>> {log}
-        (cd resources/antismash_db-schema_duckdb && python init_duckdb.py --duckdb-database $duckdb --verbose db-schema $duckdb_dir) 2>> {log}
-        """
-
 rule antismash_db_duckdb:
     input:
-        duckdb = "data/processed/{name}/antismash_database/antiSMASH_{version}_DB_SCHEMA",
         antismash=lambda wildcards: expand("data/processed/{name}/antismash/{version}/{strains}",
             name=wildcards.name,
             version=wildcards.version,
@@ -32,7 +13,8 @@ rule antismash_db_duckdb:
     log:
         "logs/antismash_db-duckdb/{name}_{version}.log",
     params:
-        prefix=lambda wildcards: find_common_prefix(f"data/processed/{wildcards.name}/antismash/{wildcards.version}/")
+        prefix=lambda wildcards: find_common_prefix(f"data/processed/{wildcards.name}/antismash/{wildcards.version}/"),
+        duckdb = "resources/antismash_db-schema_duckdb/duckdb-schema/antismash_db.duckdb",
     shell:
         """
         set -e
@@ -47,6 +29,6 @@ rule antismash_db_duckdb:
         export PATH="$HOME/.cargo/bin:$PATH"
 
         # Run the full_workflow.sh script
-        duckdb=$PWD/{input.duckdb}/antismash_db.duckdb
+        duckdb=$PWD/{params.duckdb}
         (cd {output.database} && bash $bash_script -p {params.prefix} $input_dir -d $duckdb) &>> {log}
         """
